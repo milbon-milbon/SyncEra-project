1c347d9cc089d9af66c55e673e46ea89a2f5c647
Hitomi Uchihi <pecobitaro@gmail.com>
2024-08-03 20:40:19 +0900

    ログ追加

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,14 @@
+import logging
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from .routers import frontend_requests, slack_requests
+
+load_dotenv()
+
+log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+logging.basicConfig(level=log_level, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
