eb8c4a829f776a43203145b451744145240e6bba
Hitomi Uchihi <pecobitaro@gmail.com>
2024-08-06 06:07:11 +0900

    引数をuser_id => slack_user_idに記載変更、Unixタイムスタンプへの変換処理追加

--- backend/app/services/make_advices.py
+++ backend/app/services/make_advices.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 def make_advices(slack_user_id, start_date, end_date):
     try:
-        employee_info = get_employee_info(employee_id) # 引数で受け取るかクエリで取得するか
+        employee_info = get_employee_info(slack_user_id) # 引数で受け取るかクエリで取得するか
         daily_report = get_daily_report(slack_user_id, start_date, end_date)
         times_tweet = get_times_tweet(slack_user_id, start_date, end_date)
         summary = make_summarize_report(slack_user_id, start_date, end_date)
