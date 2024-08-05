a6ae55e2927774362ef3fc76cf68ecd7c1083098
Hitomi Uchihi <pecobitaro@gmail.com>
2024-08-05 14:59:54 +0900

    アンケート関連以外のテーブル作成完了

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -45,3 +45,19 @@ class TimesTweet(Base):
     __tablename__ = 'times_tweet'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    channel_id = Column(String, ForeignKey('times_list.channel_id'), nullable=False)
+    user_id = Column(String(100), ForeignKey('slack_user_info.id'), nullable=False)
+    text = Column(Text, nullable=False)
+    ts = Column(String, nullable=False)
+    thread_ts = Column(String, nullable=True)
+    parent_user_id = Column(String, nullable=True)
+    edited = Column(String, nullable=True)
+    edited_by = Column(String(100), nullable=True)
+    edited_ts = Column(String, nullable=True)
+
+class Channel(Base):
+    __tablename__ = 'times_list'
+
+    user_id = Column(String(100), ForeignKey('slack_user_info.id'), nullable=False)
+    channel_name = Column(String(100), nullable=False)
+    channel_id = Column(String(100), primary_key=True, nullable=False)
