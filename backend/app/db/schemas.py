b50e936c6c22d72d5ce8e243368a51c30b882f9a
Hitomi Uchihi <pecobitaro@gmail.com>
2024-08-12 18:52:46 +0900

    キャリアアンケートの設問、回答選択肢をseedingに追加

--- backend/app/db/schemas.py
+++ backend/app/db/schemas.py
@@ -81,26 +81,26 @@ class ContactForm(ContactFormBase):
 '''
 キャリアアンケート用
 '''
-
 class QuestionBase(BaseModel):
     question_text: str
     choice_a: Optional[str] = None
     choice_b: Optional[str] = None
     choice_c: Optional[str] = None
     choice_d: Optional[str] = None
+    next_question_a_id: Optional[int] = None
+    next_question_b_id: Optional[int] = None
+    next_question_c_id: Optional[int] = None
+    next_question_d_id: Optional[int] = None
 
 class QuestionCreate(QuestionBase):
     pass
 
 class Question(QuestionBase):
     id: int
-    next_question_a_id: Optional[int] = None
-    next_question_b_id: Optional[int] = None
-    next_question_c_id: Optional[int] = None
-    next_question_d_id: Optional[int] = None
 
     class Config:
         orm_mode = True
+
 
 class ResponseBase(BaseModel):
     question_id: int
